5f47594d61a9b3836f28d2dd7bb69bdb0f3541b6
madcow95 <lobasketve@naver.com>
2021-04-08 22:03:53 +0900

    test

---
+++ Home/src/com/web/main/test.java
@@ -0,0 +1,15 @@
+package com.web.main;
+
+public class test {
+
+	public static void main(String args[]) {
+		
+		System.out.println("this is test code");
+		
+		for(int i = 0; i < 10; i++) {
+			
+			System.out.println(i);
+		}
+	}
+	
+}
