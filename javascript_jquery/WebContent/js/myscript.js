737976536836586f4c87807fa55d9827d453058e
madcow95 <lobasketve@naver.com>
2021-04-13 21:49:14 +0900

    javascript

--- javascript_jquery/WebContent/js/myscript.js
+++ javascript_jquery/WebContent/js/myscript.js
@@ -1,7 +1,13 @@
 /**
  * javascript에서의 주석처리
- */ 
+ */
 
 // 이렇게도 가능
+/* document.write("자바스크립트 가능"); */
+function cal() {
+	var firstNum = document.frm.firstNum.value;
+	var secondNum = document.frm.secondNum.value;
+	var sum = parseInt(firstNum) + parseInt(secondNum);
 
-/*document.write("자바스크립트 가능");*/
+	document.frm.sum.value = sum;
+}
